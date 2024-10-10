015eda5b248214db2c451800e9d47c9178baf7f8
soeun2537 <soeun2537@gmail.com>
2024-10-10 20:45:29 +0900

    feat: OAuth 제공자로부터 AccessToken 발급 및 사용자 정보 조회 기능 구현 #3

---
+++ src/main/java/ploting_server/ploting/core/security/service/oauth/OAuthService.java
@@ -0,0 +1,15 @@
+package ploting_server.ploting.core.security.service.oauth;
+
+import ploting_server.ploting.member.dto.request.OAuthAuthorizationRequest;
+import ploting_server.ploting.member.dto.request.OAuthLoginRequest;
+import ploting_server.ploting.member.dto.server.MemberLoginDto;
+
+/**
+ * OAuth 인증 서비스 인터페이스입니다.
+ */
+public interface OAuthService {
+
+    String getAccessToken(OAuthAuthorizationRequest oAuthAuthorizationRequest);
+
+    MemberLoginDto getUserInfo(OAuthLoginRequest oAuthLoginRequest);
+}
