5ba7f53ef3eeaade1d034302aacd260ea092ebbf
soeun2537 <soeun2537@gmail.com>
2024-10-10 20:55:54 +0900

    feat: OAuth 인증 및 JWT 토큰 발급, 갱신 로직 구현 #3

---
+++ src/main/java/ploting_server/ploting/member/dto/server/MemberLoginDto.java
@@ -0,0 +1,20 @@
+package ploting_server.ploting.member.dto.server;
+
+import lombok.Builder;
+import lombok.Getter;
+import ploting_server.ploting.member.entity.ProviderType;
+import ploting_server.ploting.member.entity.RolesType;
+
+/**
+ * OAuth 로그인 요청 시 사용되는 회원 정보 DTO 클래스입니다.
+ */
+@Getter
+@Builder
+public class MemberLoginDto {
+
+    private final String oauthId;
+    private final ProviderType provider;
+    private final String name;
+    private final String profileImageUrl;
+    private final RolesType roles;
+}
