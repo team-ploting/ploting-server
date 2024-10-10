c6a75dcad3f67dfd3bb2e76d4852c5d666c964b7
soeun2537 <soeun2537@gmail.com>
2024-10-10 20:46:27 +0900

    feat: Kakao 및 Naver 사용자 정보 DTO 추가 #3

---
+++ src/main/java/ploting_server/ploting/core/security/dto/oauth/NaverUserInfoDto.java
@@ -0,0 +1,40 @@
+package ploting_server.ploting.core.security.dto.oauth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+/**
+ * Naver 사용자 정보 DTO 클래스입니다.
+ */
+@Getter
+public class NaverUserInfoDto {
+
+    @JsonProperty("resultcode")
+    @NotNull
+    private String resultCode;
+
+    @JsonProperty("message")
+    @NotNull
+    private String message;
+
+    @JsonProperty("response")
+    @NotNull
+    private Response response;
+
+    @Getter
+    public static class Response {
+
+        @JsonProperty("id")
+        @NotNull
+        private String oauthId;
+
+        @JsonProperty("name")
+        @NotNull
+        private String name;
+
+        @JsonProperty("profile_image")
+        @NotNull
+        private String profileImageUrl;
+    }
+}
