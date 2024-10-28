b0cc3c3a032cc774ff3832a1e042d8bcc7a01d7f
soeun2537 <soeun2537@gmail.com>
2024-10-28 17:36:32 +0900

    feat: Jwt 토큰 헤더에 추가 #16

---
+++ src/main/java/ploting_server/ploting/core/security/constant/JwtConstants.java
@@ -0,0 +1,11 @@
+package ploting_server.ploting.core.security.constant;
+
+/**
+ * JWT 관련 설정 값을 관리하는 클래스입니다.
+ */
+public class JwtConstants {
+
+    public static final String ACCESS_TOKEN_HEADER = "Authorization";
+    public static final String REFRESH_TOKEN_HEADER = "Refresh-Token";
+    public static final String TOKEN_PREFIX = "Bearer ";
+}
