1a98bee48528cc63ac968f79ef90ec468dfafc17
soeun2537 <soeun2537@gmail.com>
2024-11-14 11:09:06 +0900

    feat: 모임 관련 에러 처리 #45

---
+++ src/main/java/ploting_server/ploting/core/code/error/MeetingErrorCode.java
@@ -0,0 +1,29 @@
+package ploting_server.ploting.core.code.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import ploting_server.ploting.core.response.ErrorResponse;
+
+/**
+ * 모임 관련 에러 코드를 관리하는 enum 클래스입니다.
+ */
+@Getter
+public enum MeetingErrorCode implements BaseErrorCode {
+    // 404 NOT_FOUND
+    NOT_FOUND_MEETING_ID(404, "존재하지 않는 단체입니다.", HttpStatus.NOT_FOUND);
+
+    private final int errorCode;
+    private final String errorMessage;
+    private final HttpStatus status;
+
+    MeetingErrorCode(int errorCode, String errorMessage, HttpStatus status) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.status = status;
+    }
+
+    @Override
+    public ErrorResponse getErrorResponse() {
+        return new ErrorResponse(this.errorCode, this.errorMessage);
+    }
+}
