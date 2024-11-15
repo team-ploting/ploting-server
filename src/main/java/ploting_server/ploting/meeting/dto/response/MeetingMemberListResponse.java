05ca07bc6c329457cb6963c9ddcebfefa361896b
soeun2537 <soeun2537@gmail.com>
2024-11-15 17:06:06 +0900

    fix: 모임 멤버 조회 수정 #46

--- src/main/java/ploting_server/ploting/meeting/dto/response/MeetingMemberListResponse.java
+++ src/main/java/ploting_server/ploting/meeting/dto/response/MeetingMemberListResponse.java
@@ -19,9 +19,6 @@ public class MeetingMemberListResponse {
     @Schema(description = "멤버의 레벨", example = "1")
     private final int level;
 
-    @Schema(description = "멤버의 한 줄 소개", example = "한 줄 소개")
-    private final String introduction;
-
     @Schema(description = "단체장 여부", example = "true")
     private final boolean leaderStatus;
 }
