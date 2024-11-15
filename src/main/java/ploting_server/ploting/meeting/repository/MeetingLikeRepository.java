a61d264ea782162b4c795668ed4d0d1d10f483bd
soeun2537 <soeun2537@gmail.com>
2024-11-15 18:24:19 +0900

    feat: 모임의 좋아요 취소 API 개발 #47

--- src/main/java/ploting_server/ploting/meeting/repository/MeetingLikeRepository.java
+++ src/main/java/ploting_server/ploting/meeting/repository/MeetingLikeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ploting_server.ploting.meeting.entity.MeetingLike;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * MeetingLike의 Repository 입니다.
@@ -12,4 +13,6 @@ public interface MeetingLikeRepository extends JpaRepository<MeetingLike, Long> 
     List<MeetingLike> findAllByMeetingId(Long meetingId);
 
     boolean existsByMemberIdAndMeetingId(Long memberId, Long meetingId);
+
+    Optional<MeetingLike> findByMemberIdAndMeetingId(Long memberId, Long meetingId);
 }
