bf689c51c3c91aa324f89e037c0d3a93a2a52c1e
soeun2537 <soeun2537@gmail.com>
2024-11-15 20:19:41 +0900

    feat: 게시글 관련 예외 처리 추가 #49

---
+++ src/main/java/ploting_server/ploting/core/code/error/PostErrorCode.java
@@ -0,0 +1,30 @@
+package ploting_server.ploting.core.code.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import ploting_server.ploting.core.response.ErrorResponse;
+
+/**
+ * 게시글 관련 에러 코드를 관리하는 enum 클래스입니다.
+ */
+@Getter
+public enum PostErrorCode implements BaseErrorCode {
+    // 404 NOT_FOUND
+    NOT_FOUND_POST_ID(404, "존재하지 않는 게시글입니다.", HttpStatus.NOT_FOUND),
+    ;
+
+    private final int errorCode;
+    private final String errorMessage;
+    private final HttpStatus status;
+
+    PostErrorCode(int errorCode, String errorMessage, HttpStatus status) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.status = status;
+    }
+
+    @Override
+    public ErrorResponse getErrorResponse() {
+        return new ErrorResponse(this.errorCode, this.errorMessage);
+    }
+}
