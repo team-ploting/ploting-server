94e474525b65a9ee927c7cda7126988e02d54498
soeun2537 <soeun2537@gmail.com>
2024-11-17 20:59:59 +0900

    fix: Request DTO 수정 #52

--- src/main/java/ploting_server/ploting/comment/dto/request/CommentUpdateRequest.java
+++ src/main/java/ploting_server/ploting/comment/dto/request/CommentUpdateRequest.java
@@ -1,18 +1,14 @@
 package ploting_server.ploting.comment.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 /**
  * 댓글 수정 시 댓글 정보를 담는 DTO 클래스입니다.
  */
 @Getter
-@Builder
-@RequiredArgsConstructor
 public class CommentUpdateRequest {
 
     @Schema(description = "댓글 본문", example = "갑사합니다", required = true)
-    private final String content;
+    private String content;
 }
