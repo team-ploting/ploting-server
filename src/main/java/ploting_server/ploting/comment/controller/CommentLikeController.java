81b5b7616909649678993965a60c8d0849b1fd16
soeun2537 <soeun2537@gmail.com>
2024-11-17 22:00:53 +0900

    feat: 댓글 좋아요 취소 API 개발 #51

--- src/main/java/ploting_server/ploting/comment/controller/CommentLikeController.java
+++ src/main/java/ploting_server/ploting/comment/controller/CommentLikeController.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ploting_server.ploting.comment.service.CommentLikeService;
 import ploting_server.ploting.core.code.success.GlobalSuccessCode;
 import ploting_server.ploting.core.response.BfResponse;
@@ -39,6 +36,22 @@ public class CommentLikeController {
             @AuthenticationPrincipal PrincipalDetails principalDetails,
             @PathVariable Long commentId) {
         commentLikeService.pressCommentLike(Long.parseLong(principalDetails.getUsername()), commentId);
+        return ResponseEntity.ok(new BfResponse<>(GlobalSuccessCode.SUCCESS));
+    }
+
+    @Operation(
+            summary = "좋아요 취소",
+            description = "댓글의 좋아요를 취소합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 좋아요 삭제 성공",
+                    content = @Content(examples = @ExampleObject(value = "{ \"code\": 200, \"message\": \"정상 처리되었습니다.\" }")))
+    })
+    @DeleteMapping("{commentId}/like")
+    public ResponseEntity<BfResponse<GlobalSuccessCode>> cancelCommentLike(
+            @AuthenticationPrincipal PrincipalDetails principalDetails,
+            @PathVariable Long commentId) {
+        commentLikeService.cancelCommentLike(Long.parseLong(principalDetails.getUsername()), commentId);
         return ResponseEntity.ok(new BfResponse<>(GlobalSuccessCode.SUCCESS));
     }
 }
