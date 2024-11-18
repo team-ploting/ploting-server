ce28e3b058856a137e6954bc1644d90e332c4553
soeun2537 <soeun2537@gmail.com>
2024-11-18 19:24:47 +0900

    feat: 모든 게시글과 모임 조회 API 검색 기능 추가 #48

--- src/main/java/ploting_server/ploting/post/repository/PostRepository.java
+++ src/main/java/ploting_server/ploting/post/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package ploting_server.ploting.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ploting_server.ploting.post.entity.Post;
 
 import java.util.List;
@@ -8,6 +10,8 @@ import java.util.List;
 /**
  * 게시글 도메인의 Repository 입니다.
  */
-public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByMemberId(Long memberId);
+public interface
+PostRepository extends JpaRepository<Post, Long> {
+    @Query("SELECT p FROM Post p WHERE :search IS NULL OR LOWER(p.title) LIKE LOWER(CONCAT('%', :search, '%')) OR LOWER(p.content) LIKE LOWER(CONCAT('%', :search, '%'))")
+    List<Post> searchPosts(@Param("search") String search);
 }
