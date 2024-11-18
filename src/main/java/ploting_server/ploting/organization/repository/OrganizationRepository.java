5fb06cbdaddda51f6a0f038af7622ce6b718c4fc
soeun2537 <soeun2537@gmail.com>
2024-11-18 21:17:02 +0900

    feat: 단체 조회 페이징 제거 #73

--- src/main/java/ploting_server/ploting/organization/repository/OrganizationRepository.java
+++ src/main/java/ploting_server/ploting/organization/repository/OrganizationRepository.java
@@ -1,17 +1,18 @@
 package ploting_server.ploting.organization.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ploting_server.ploting.organization.entity.Organization;
+
+import java.util.List;
 
 /**
  * 단체 도메인의 Repository 입니다.
  */
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-    // TODO: N+1 문제 처리
-    @Query("select o from Organization o join o.organizationMembers om where om.member.id = :memberId")
-    Page<Organization> findAllByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+    @Query("SELECT o FROM Organization o ORDER BY o.createdAt DESC")
+    List<Organization> findAllOrderByCreatedAtDesc();
+
+    @Query("select o from Organization o join o.organizationMembers om where om.member.id = :memberId ORDER BY o.createdAt DESC")
+    List<Organization> findAllByMemberIdOrderByCreatedAtDesc(Long memberId);
 }
