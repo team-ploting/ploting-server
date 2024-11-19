cd70a443dbe3940a691c1038c15359534cfd35f6
soeun2537 <soeun2537@gmail.com>
2024-11-19 16:32:14 +0900

    docs: PointInfoResponse 문서 수정 #80

--- src/main/java/ploting_server/ploting/point/dto/response/PointInfoResponse.java
+++ src/main/java/ploting_server/ploting/point/dto/response/PointInfoResponse.java
@@ -14,18 +14,18 @@ import ploting_server.ploting.point.entity.LevelType;
 @RequiredArgsConstructor
 public class PointInfoResponse {
 
-    @Schema(description = "현재 레벨에서의 포인트", example = "20")
+    @Schema(description = "현재 레벨에서의 포인트", example = "10")
     private final int currentLevelPoints;
 
     @Schema(description = "다음 레벨까지 포인트", example = "30")
     private final int nextLevelPoints;
 
-    @Schema(description = "레벨", example = "2")
+    @Schema(description = "레벨", example = "3")
     private final int level;
 
     @Schema(description = "레벨 타입", example = "씨앗")
     private final LevelType levelType;
 
-    @Schema(description = "포인트 합계", example = "50")
+    @Schema(description = "포인트 합계", example = "100")
     private final int totalPoints;
 }
