a16de28d2388f30e40be11b17847e0355846a8e3
soeun2537 <soeun2537@gmail.com>
2024-11-26 02:32:14 +0900

    feat: 응답 스키마 변경 #91

--- src/main/java/ploting_server/ploting/core/response/BfResponse.java
+++ src/main/java/ploting_server/ploting/core/response/BfResponse.java
@@ -1,9 +1,9 @@
 package ploting_server.ploting.core.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import ploting_server.ploting.core.code.success.GlobalSuccessCode;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import ploting_server.ploting.core.code.success.GlobalSuccessCode;
 
 /**
  * 프로젝트 전역적으로 사용되는 REST API 응답 클래스입니다.
@@ -15,7 +15,7 @@ public class BfResponse<T> {
     @Schema(description = "응답 코드", example = "200")
     private int code;
 
-    @Schema(description = "응답 메시지", example = "성공")
+    @Schema(description = "응답 메시지", example = "정상 처리되었습니다.")
     private String message;
 
     @Schema(description = "응답 데이터")
