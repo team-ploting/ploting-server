e1c20b7410585bb63e500c03d4e9c845fecec802
soeun2537 <soeun2537@gmail.com>
2024-11-30 22:19:01 +0900

    fix: JSON parse 에러 대응

--- src/main/java/ploting_server/ploting/organization/dto/request/OrganizationJoinRequest.java
+++ src/main/java/ploting_server/ploting/organization/dto/request/OrganizationJoinRequest.java
@@ -1,14 +1,12 @@
 package ploting_server.ploting.organization.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Getter;
 
 /**
  * 단체 가입 시 가입 정보를 담는 DTO 클래스입니다.
  */
 @Getter
-@Builder
 public class OrganizationJoinRequest {
 
     @Schema(description = "가입 한줄 소개", example = "안녕하세요", required = true)
